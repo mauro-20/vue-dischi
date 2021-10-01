d1fb5585857e42798e0f6cbba3d0fafd26c697dd
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-10-01 15:34:58 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <div class="logo">
+      <img :src="src" alt="Logo">
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: ['src']
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../assets/style/variables';
+  .logo{
+    background-color: $headerBgColor;
+    padding: .625rem 1.25rem;
+
+    img{
+      width: 3.125rem;
+    }
+  }
+
+</style>
